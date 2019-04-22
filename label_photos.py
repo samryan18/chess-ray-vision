03881de56baff858a0f822e92c23f70b2fcbabf4
Sam <saml.ryan@yahoo.com>
2019-04-21 20:58:16 -0400

    label photos code

---
+++ label_photos.py
@@ -0,0 +1,32 @@
+import os
+
+with open('labels.txt') as f:
+    labels = f.readlines()
+labels = [x.strip().replace('\n','').replace(' ','').replace('\t','') 
+                    for x in labels]
+
+print(f'Found {len(labels)} labels.')
+
+filename_labels = []
+label_counts = {}
+for label in labels:
+    new_label = label
+    if label in label_counts.keys():
+        label_counts[label] = label_counts[label]+1
+        new_label = f'{new_label}_{label_counts[label]}'
+    else:
+        label_counts[label] = 1
+
+    filename_labels.append(new_label)
+
+
+print(f'Made the {len(filename_labels)} labels unique.')
+
+path_to_photos = 'photos'
+
+for i,fname in enumerate(os.listdir(path_to_photos)):
+    print(fname)
+    ext = fname.split('.')[-1]
+    src = f'{path_to_photos}/{fname}'
+    dest = f'{path_to_photos}/{filename_labels[i]}.{ext}'
+    os.rename(src, dest) 
